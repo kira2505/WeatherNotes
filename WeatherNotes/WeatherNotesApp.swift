23a6a63c6b88136a2845236f6e2f54a55157ab53
Kira <kirass0525@gmail.com>
2025-12-08 16:04:06 +0100

    Initial Commit

---
+++ WeatherNotes/WeatherNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherNotesApp.swift
+//  WeatherNotes
+//
+//  Created by Кира Суботовская on 08/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
